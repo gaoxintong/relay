f0d753409b9ab7078cb3d588fb279dc13af959d2
luzhenqian <15753140326@163.com>
2021-02-04 23:51:38 +0800

    fix: main

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"relay/server"
+	"relay/gateway"
 )
 
 func main() {
-	server.Run()
+	TCPAddress := "192.168.2.136:9001"     // tcp 监听服务地址
+	IOTHubAddress := "39.98.250.155:18100" // iot 平台地址
+	productKey := "abdf6b26a399494869c5db5476d1d617fdb5f7d6579fd093ccf78c77ea61e70f"
+	deviceName := "relay"
+	version := "1.0.0"
+	g := gateway.New(TCPAddress, IOTHubAddress, productKey, deviceName, version)
+	if err := g.Run(); err != nil {
+		panic(err)
+	}
 }
