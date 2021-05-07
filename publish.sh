3df09dc0dc320b3c1cd002beda9776720123127f
luzhenqian <15753140326@163.com>
2021-05-07 11:21:41 +0800

    chore: publish.sh

--- publish.sh
+++ publish.sh
@@ -1,1 +1,4 @@
-scp -P 9022 ./prg root@123.232.115.10:/root/services/prg/
+./build.sh
+ssh root@123.232.115.10 -p 9022 "/root/services/prg/kill.sh"
+scp -P 9022 ./prg root@123.232.115.10:/root/services/prg
+ssh root@123.232.115.10 -p 9022 "/root/services/prg.sh"
