a4d0351d70560aa8c0b5890578cb9596012bf51b
luzhenqian <15753140326@163.com>
2021-05-11 14:12:31 +0800

    feat: use configuration as startup arguments

--- main.go
+++ main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"relay/gateway"
 )
 
 func main() {
-	// TCPAddress := "192.168.2.136:9001"     // tcp 监听服务地址
-	TCPAddress := "0.0.0.0:5000"           // tcp 监听服务地址
-	IOTHubAddress := "39.98.250.155:18100" // iot 平台地址
-	productKey := "3e67c6e104b920cbc5da9fa8d669d5af36bf672d84cfbce62230addea9de1f6eed89e9ba89ab267aa72c3608a0bd1141"
-	deviceName := "relay-test"
-	//deviceName := "relay"
-	version := "1.0.0"
+	TCPAddress := config.TCPAddress
+	IOTHubAddress := config.IOTHubAddress
+	productKey := config.ProductKey
+	deviceName := config.DeviceName
+	version := config.Version
 	g := gateway.New(TCPAddress, IOTHubAddress, productKey, deviceName, version)
 	if err := g.Run(); err != nil {
+		panic(err)
+	}
+}
+
+type Config struct {
+	TCPAddress    string `yaml:"TCPAddress"`
+	IOTHubAddress string `yaml:"IOTHubAddress"`
+	ProductKey    string `yaml:"productKey"`
+	DeviceName    string `yaml:"deviceName"`
+	Version       string `yaml:"version"`
+}
+
+var config = &Config{}
+
+func init() {
+	yamlFile, err := ioutil.ReadFile("./config/config.yaml")
+	if err != nil {
+		panic(err)
+	}
+	err = yaml.Unmarshal(yamlFile, config)
+	if err != nil {
 		panic(err)
 	}
 }
